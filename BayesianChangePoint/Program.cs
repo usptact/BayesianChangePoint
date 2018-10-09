50293a8ab27a8464cc693a545f069facf01c3fd1
Vladislavs Dovgalecs <vladislavsd@speak.ai>
2018-10-08 17:11:18 -0700

    Update to use open source Infer.NET

--- BayesianChangePoint/Program.cs
+++ BayesianChangePoint/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MicrosoftResearch.Infer;
-using MicrosoftResearch.Infer.Models;
-using MicrosoftResearch.Infer.Distributions;
+using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Distributions;
 
 namespace BayesianChangePoint
 {
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ The idea is to model the data sequence before a switchpoint using a Poisson rand
 The priors for both \lambda_early and \lambda_late is Gamma(2.0, 2.0)
 
 The current model is implemented in Infer.NET 2.6 that has a bug and has been addressed with a workaround.
+
+Updated to use open source Infer.NET.
